1e4e1472a7e69e83ea2d5da6d3687f727cd81d38
MrPickle <nimsarapiumalsac@gmail.com>
2024-08-12 15:50:15 +0530

    user schema

---
+++ backend/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { isEmail } = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, "please enter an email"],
+    lowercase: true,
+    unique: true,
+    validate: [isEmail, "please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "please enter a password"],
+    minlength: [6, "minimum password length is 6"],
+  },
+  birthdate: { type: Date },
+  district: { type: String, required: true }, 
+  division: { type: String, required: true },
+  nationalId: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
