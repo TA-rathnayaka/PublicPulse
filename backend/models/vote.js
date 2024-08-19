555c2ef37e0389daa4a7ccaffde2b85670ddbfa2
MrPickle <nimsarapiumalsac@gmail.com>
2024-08-19 16:17:20 +0530

    poll routes added

--- backend/models/vote.js
+++ backend/models/vote.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const voteSchema = new Schema({
-  pollId: { type: Schema.Types.ObjectId, ref: 'Poll', required: true },
-  optionId: { type: Schema.Types.ObjectId, ref: 'Option', required: true },
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  timestamp: { type: Date, default: Date.now }
+  pollId: { type: Schema.Types.ObjectId, ref: "Poll", required: true },
+  optionId: { type: Schema.Types.ObjectId, ref: "Option", required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  timestamp: { type: Date, default: Date.now },
 });
 
-const Vote = mongoose.model('Vote', voteSchema);
+const Vote = mongoose.model("Vote", voteSchema);
 module.exports = Vote;
