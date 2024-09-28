5a3adaab5dac8c332165dbbe4f8feadf13491a13
MrPickle <nimsarapiumalsac@gmail.com>
2024-09-28 11:18:49 +0530

    basic user login adn signup

--- backend-v2/src/index.js
+++ backend-v2/src/index.js
@@ -1,23 +1,11 @@
 const express = require('express');
-const pool = require('../config/db');
+const AuthRoutes = require ('../routes/AuthRoutes')
 const bcrypt = require('bcrypt');
 const app = express();
 
 app.use(express.json());
 
-app.post('/register', async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const result = await pool.query(
-      'INSERT INTO Users (email, password) VALUES ($1, $2) RETURNING UserId',
-      [email, hashedPassword]
-    );
-    res.status(201).json({ userId: result.rows[0].userid });
-  } catch (error) {
-    res.status(500).json({ message: 'Error registering user', error });
-  }
-});
+app.use('/auth', AuthRoutes);
 
 app.get('/users', async (req, res) => {
     try {
