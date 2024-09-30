bf46a3ddcca63891012abbe79b6babce24634b8a
Chathura Kumara <chathurakumara@192.168.1.7>
2024-09-30 22:18:09 +0530

    Removed unnecessary files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
+<<<<<<< HEAD
 # PublicPulse
 "PublicPulse" is a decentralized app that enables users to share and vote on opinions about various policies. The app ensures transparency and fairness by leveraging Web3 technology, providing a secure and anonymous platform for users to engage in discussions and influence policy decisions.
+=======
+# client
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+>>>>>>> ef4ef5b (Added Poll Dashboard and CustomBottomNavigationBar)
---
+++ lib/components/poll_dashboard_item.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:client/screens/poll_screen.dart';
+
+class PollCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String label;
+  final String avatarLabel;
+  final Color avatarBackgroundColor;
+  final String imageUrl;
+
+  // Constructor for PollCard to accept dynamic values
+  PollCard({
+    required this.title,
+    required this.subtitle,
+    required this.label,
+    required this.avatarLabel,
+    this.avatarBackgroundColor = Colors.black12,
+    this.imageUrl = '', // You can provide a default image if needed
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: (){
+        Navigator.pushNamed(context,PollScreen.id);
+      },
+      style: ElevatedButton.styleFrom(
+
+        padding: EdgeInsets.all(16.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0), // Rounded edges
+        ),// Adjust padding if needed
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Image on the left
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(8),
+                image: imageUrl.isNotEmpty
+                    ? DecorationImage(
+                        image: NetworkImage(imageUrl),
+                        fit: BoxFit.cover,
+                      )
+                    : null, // If imageUrl is empty, no image is shown
+              ),
+            ),
+            SizedBox(width: 12),
+            // Poll details in a column
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title, // Dynamic Title
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    subtitle, // Dynamic Subtitle
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                  SizedBox(height: 6),
+                  Text(
+                    label, // Dynamic Label
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  SizedBox(height: 6),
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 10,
+                        backgroundColor:
+                            avatarBackgroundColor, // Dynamic avatar color
+                      ),
+                      SizedBox(width: 8),
+                      Text(avatarLabel), // Dynamic Avatar Label
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
