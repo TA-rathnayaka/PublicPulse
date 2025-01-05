02991683c4b43a38a6211f7133cb851c38da8abf
MrPickle <nimsarapiumalsac@gmail.com>
2025-01-05 19:08:03 +0530

    deleted client

--- client/lib/views/constants/profile_constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:client/views/constants/constants.dart';
-import 'package:flutter/material.dart';
-
-const double kPaddingHorizontalUserProfileScreen = 16.0;
-const Color kFillColorUserProfileScreen = kTextFieldBackgroundColor;
-const double kCircleAvatarRadiusUserProfileScreen = 50.0;
-const double kEditIconRadiusUserProfileScreen = 15.0;
-const TextStyle kLabelTextStyle = kInputHintTextStyle;
-InputDecoration kInputDecoration = kTextFieldDecoration;
