4849092d2587a7ae0aa3f4196b683e49840f200f
Tharanga Anuradha <104713256+TA-rathnayaka@users.noreply.github.com>
2025-01-06 11:43:01 +0530

    check polls availability

--- client/lib/views/screens/polls_dashboard.dart
+++ client/lib/views/screens/polls_dashboard.dart
@@ -42,7 +42,6 @@ class PollDashboard extends StatelessWidget {
               const SizedBox(height: 20),
               Consumer<PollsProvider>(
                 builder: (context, pollsProvider, child) {
-                  // Check if the polls list is not null or empty
                   if (pollsProvider.polls == null || pollsProvider.polls.isEmpty) {
                     return Center(child: Text("No polls available."));
                   }
